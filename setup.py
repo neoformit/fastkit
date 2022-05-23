b416ee9650a51e3c07b8188e005b27df3d076bee
Cameron Hyde <chyde@neoformit.com>
2022-05-23 11:59:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Setup packages."""
+
+from distutils.core import setup
+
+setup(
+    name='FastKit',
+    version='0.1.0',
+    description='Biological data preprocessing facility',
+    author='Cameron Hyde',
+    author_email='chyde@neoformit.com',
+    url='https://github.com/neoformit/fastkit',
+    packages=['', 'lib'],
+)
